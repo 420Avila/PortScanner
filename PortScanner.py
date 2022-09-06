9a21900a5362332242644cb4ec67ec5c8ec5d46d
Oswaoldo Hernández Ávila <108857178+420Avila@users.noreply.github.com>
2022-09-05 23:24:46 -0500

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -3,7 +3,15 @@ from re import S
 import sys 
 import socket
 from unittest import result
-
+print()
+print(" _  _ ___   ___              _ _       ")
+print("| || |__ \ / _ \            (_) |      ")
+print("| || |_ ) | | | | __ ___   ___| | __ _ ")
+print("|__   _/ /| | | |/ _` \ \ / / | |/ _` |")
+print("   | |/ /_| |_| | (_| |\ V /| | | (_| |")
+print("   |_|____|\___/ \__,_| \_/ |_|_|\__,_|")
+print()
+print() 
 objetivo = socket.gethostbyname(input("Insertar la dirección IP: "))
 
 print("Escaneando Objetivo: " + objetivo)
